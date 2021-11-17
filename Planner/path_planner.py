372906ff1ade03670a741b25c54762a3906cda40
Miracle-qi <929798194@qq.com>
2021-11-17 10:24:18 +0800

    delete

--- Planner/path_planner.py
+++ Planner/path_planner.py
@@ -14,3 +14,18 @@ def following_path(wp_x, wp_y, d_dist):
         ryaw.append(cubicspline.calc_yaw(i_s))
         rk.append(cubicspline.calc_curvature(i_s))
     return rx, ry, ryaw, rk, cubicspline
+
+
+
+def laneChanging_path(, , d_dist):
+    cubicspline = cubic_spline.Spline2D(wp_x, wp_y)
+    s = np.arange(0, cubicspline.s[-1], d_dist)
+
+    rx, ry, ryaw, rk = [], [], [], []
+    for i_s in s:
+        ix, iy = cubicspline.calc_position(i_s)
+        rx.append(ix)
+        ry.append(iy)
+        ryaw.append(cubicspline.calc_yaw(i_s))
+        rk.append(cubicspline.calc_curvature(i_s))
+    return rx, ry, ryaw, rk, cubicspline
